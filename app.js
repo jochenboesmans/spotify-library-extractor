c9f551120a03c6b98a82a32fa178d97763624728
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-11-17 00:37:18 +0100

    Remove unnecessary scopes.

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-library-read user-library-modify';
+  var scope = 'user-read-private user-read-email user-library-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
